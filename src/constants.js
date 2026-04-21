68bc81367a38ecf3c75a2e5da871ad13f2c7e90f
Zach Hammer <zachary_hammer@alumni.brown.edu>
2026-04-20 21:06:03 -0400

    feat: tile grass and sand map layers with zone constants

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const MAP_W = 480;
+export const MAP_H = 360;
+export const WAVE_H = 80;
+export const SAND_H = 100;
+export const GRASS_TOP = 0;
+export const SAND_TOP = MAP_H - WAVE_H - SAND_H;
+export const WAVE_TOP = MAP_H - WAVE_H;
--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,8 +1,21 @@
 import Phaser from 'phaser';
+import { MAP_W, SAND_TOP, SAND_H, GRASS_TOP } from '../constants.js';
 
 export class MainScene extends Phaser.Scene {
   constructor() { super('MainScene'); }
-  preload() {}
-  create() {}
+
+  preload() {
+    this.load.image('grass', 'assets/grass-tile.png');
+    this.load.image('beach', 'assets/beach-tile.png');
+  }
+
+  create() {
+    this.add.tileSprite(0, GRASS_TOP, MAP_W, SAND_TOP - GRASS_TOP, 'grass')
+      .setOrigin(0, 0);
+
+    this.add.tileSprite(0, SAND_TOP, MAP_W, SAND_H, 'beach')
+      .setOrigin(0, 0);
+  }
+
   update() {}
 }
